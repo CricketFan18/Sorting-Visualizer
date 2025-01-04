9ca4bcc0561d6ef52dd4c4ccca87c0f3ca72a980
Vivek Kesarwani <vivekkesarwani2005@gmail.com>
2025-01-04 23:22:29 +0530

    Fixed UI

--- main.js
+++ main.js
@@ -28,18 +28,18 @@ async function bubbleSort(array) {
     const bars = document.querySelectorAll(".bar");
     for(let i = 0 ; i < array.length - 1 ; i++ ) {
         for(let j = 0 ; j < array.length - i - 1 ; j++ ) {
-            HighlightBars(bars[j] , bars[j+1]);
-            const a = await nextStep();
+            await HighlightBars(bars[j] , bars[j+1]);
+            await nextStep();
             if(array[j] > array[j+1])
             {
-                const c = await canSwap(bars[j] , bars[j+1]);
+                await canSwap(bars[j] , bars[j+1]);
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];  
             }
-            UnHighlightBars(bars[j] , bars[j+1])
+            await UnHighlightBars(bars[j] , bars[j+1])
         }
-        bars[bars.length - i - 1].style.backgroundColor = "green";
+        bars[bars.length - i - 1].classList.add("sorted");
     }    
-    bars[0].style.backgroundColor = "green";
+    bars[0].classList.add("sorted");
 }
 
 function nextStep() {
@@ -59,36 +59,50 @@ function canSwap(bar1,bar2) {
             const tempText = bar1.textContent;
             bar1.textContent = bar2.textContent;
             bar2.textContent = tempText;
-            console.log("swap animation...");
             resolve(true);
-        },200);                
+        },100);                
     });
 }
 
-function HighlightBars(bar1,bar2) {    
-    bar1.classList.add("highlighted");
-    bar2.classList.add("highlighted");
+function HighlightBars(bar1,bar2) {   
+    return new Promise((resolve)=>{
+        setTimeout(()=> {
+            bar1.classList.add("highlighted");
+            bar2.classList.add("highlighted");
+            resolve();
+        },100); 
+    })       
 }
 
 function UnHighlightBars(bar1,bar2) {
-    bar1.classList.remove("highlighted");
-    bar2.classList.remove("highlighted");
+    return new Promise((resolve)=>{
+        setTimeout(()=> {
+            bar1.classList.remove("highlighted");
+            bar2.classList.remove("highlighted");
+            resolve();
+        },200); 
+    })    
 }
 
 
 bubbleSortButton.addEventListener("click",()=> { 
     containerEl.innerHTML = "";
+    clearInterval(automate);
     let shuffledNumber = shuffleArray(numbers);
     renderArray(shuffledNumber);
-    bubbleSort(shuffledNumber);
+    setTimeout(()=> {
+        bubbleSort(shuffledNumber);
+    },300);    
 })
 
 autoButton.addEventListener("click",()=> {
     automate = setInterval(()=>{
         nextButton.click();
-    },500);
+    },100);
 })
 
 stopButton.addEventListener("click",()=> {
     clearInterval(automate);
 })
+
+renderArray(numbers);
